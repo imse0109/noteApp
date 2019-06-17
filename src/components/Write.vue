c12ce22838af2029762236be1c98c305803e1d04
sclim <imse0109@naver.com>
2019-06-17 19:40:04 +0900

    5-3. write 화면 버튼 수정

--- src/components/Write.vue
+++ src/components/Write.vue
@@ -19,4 +19,5 @@ export default {
 .write .container{padding:16px 16px 72px 16px;}
 .write textarea{width:100%;height:100%;background:#fff;border:1px solid #1976d2;}
 .write .v-btn{position:fixed;bottom:0;left:0;height:56px;}
+.write .v-btn:hover{position:fixed;}
 </style>
